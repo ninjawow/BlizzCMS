3ec8b7d3a97b72d80595035f3f43a0bc7390b96f
Ninjá <rcmaster87@gmail.com>
2023-09-18 18:37:03 +0200

    001

--- application/themes/default/views/layouts/layout.php
+++ application/themes/default/views/layouts/layout.php
@@ -221,9 +221,8 @@
           <a target="_blank" href="<?= $this->config->item('social_twitter'); ?>" class="uk-icon-button uk-margin-small-right"><i class="fab fa-twitter"></i></a>
           <a target="_blank" href="<?= $this->config->item('social_youtube'); ?>" class="uk-icon-button"><i class="fab fa-youtube"></i></a>
         </div>
-        <p class="uk-text-center uk-margin-small">Copyright <i class="far fa-copyright"></i> <?= date('Y'); ?> <span class="uk-text-bold"><?= $this->config->item('website_name'); ?></span>. <?= $this->lang->line('footer_rights'); ?></p>
-        <p class="uk-text-small uk-margin-small uk-text-center">World of Warcraft® and Blizzard Entertainment® are all trademarks or registered trademarks of Blizzard Entertainment in the United States and/or other countries. These terms and all related materials, logos, and images are copyright © Blizzard Entertainment. This site is in no way associated with or endorsed by Blizzard Entertainment®.</p>
-        <p class="uk-h6 uk-text-bold uk-text-uppercase uk-margin-small uk-text-center">Proudly powered by <a target="_blank" href="https://wow-cms.com">BlizzCMS</a></p>
+        <p class="uk-text-center uk-margin-small"><span class="uk-text-bold"><?= $this->config->item('website_name'); ?></span> <i class="far fa-copyright"></i> <?= date('Y'); ?> - <?= $this->lang->line('footer_rights'); ?></p>
+        <p class="uk-h6 uk-text-bold uk-text-uppercase uk-margin-small uk-text-center">Powered By <a target="_blank" href="https://wow-cms.com">BlizzCMS</a> & <a target="_blank" href="https://www.azerothcore.org">AzerothCore</a></p>
       </div>
     </section>
   </body>
